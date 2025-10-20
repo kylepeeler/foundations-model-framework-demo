63b7f99dbf3a725c23092537ca0a2b8284a50d1f
Kyle Peeler <me@kylepeeler.com>
2025-10-20 09:39:06 -0700

    explore @Generable and @Guide macros

--- FoundationModelsCodeAlong/FoundationModelsCodeAlong/Playgrounds/Playground.swift
+++ FoundationModelsCodeAlong/FoundationModelsCodeAlong/Playgrounds/Playground.swift
@@ -10,40 +10,26 @@ import Playgrounds
 
 #Playground {
     let instructions = """
-           Your job is to create an itinerary for the user.
-           Each day needs an activity, hotel and restaurant.
+        Your job is to create an itinerary for the user.
+        """
 
-           Always include a title, a short description, and a day-by-day plan.
-           """
-    
-    // Crease a new session with the language model
     let session = LanguageModelSession(instructions: instructions)
-    
-    // Asynchronously generate a response from a text prompt
-    let response = try await session.respond(to: "Generate a 3-day itinerary to Paris, France")
-    
-    
+    let prompt = "Generate a 3-day itinerary to Paris."
+    let response = try await session.respond(to: prompt,
+                                             generating: SimpleItinerary.self)
 }
 
+// The @Generable macro makes your custom type compatible with the model.
+@Generable
+struct SimpleItinerary {
+    // The @Guide macro provides hints to the model about a property.
+    @Guide(description: "An exciting name for the trip.")
+    let title: String
 
-#Playground {
-    let model = SystemLanguageModel.default
+    @Guide(description: "A short, engaging description of the trip.")
+    let description: String
 
-    // The availability property provides detailed information on the model's state.
-    switch model.availability {
-        case .available:
-            print("Foundation Models is available and ready to go!")
-
-        case .unavailable(.deviceNotEligible):
-            print("The model is not available on this device.")
-
-        case .unavailable(.appleIntelligenceNotEnabled):
-            print("Apple Intelligence is not enabled in Settings.")
-
-        case .unavailable(.modelNotReady):
-            print("The model is not ready yet. Please try again later.")
-
-        case .unavailable(let other):
-            print("The model is unavailable for an unknown reason.")
-    }
+    @Guide(description: "A list of day-by-day plans, as simple strings.")
+    @Guide(.count(3))
+    let days: [String]
 }
